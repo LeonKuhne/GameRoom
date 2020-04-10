f09e4135762c8716ab4effcf1e0f3d23216b1f2e
Leon Kuhne <lxk1170@rit.edu>
2020-04-10 13:29:42 -0400

    changed backend endpoints to match up with aws

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-URL = "https://todo.org"
+URL = "https://me2h5lz01i.execute-api.us-east-1.amazonaws.com/dev"
+endpoint = {
+	joinRoom: URL + "/room",
+	choice: URL + "/choice"
+}
 
-new Vue({
-	el: '#app',
-	data: () => ({
-		user: "Leon",
-		choices: ["first choice", "second choice"]
-	}),
-	methods: {
-		submit: (game) => {
-			// choose this game
-			fetch(URL, {
-				method: "post",
-				body: {
-					user: this.user,
-					choices: this.choices,
-					game: this.game
-				}
-			}, (res) => {
-				console.log(res)	
-			})
+window.onload = () => {
+	// CHOICES
+	Vue.component({
+		props: {
+			choices: []
+		},
+		methods: {
+			submit: (game) => {
+				fetch(endpoint.choice, {
+					method: "post",
+					body: {
+						user: this.user,
+						game: this.game
+					}
+				}, (res) => {
+					this.choices = res.choices
+				})
+			}
 		}
-	}
-})
+	})
 
+	// APP
+	new Vue({
+		el: '#app',
+		data: () => ({
+			state: "setup",
+			enteredName: "",
+			enteredRoom: "",
+			user: "",
+			room: "",
+			choices: []
+		}),
+		methods: {
+			joinRoom: (user, room) => {
+				fetch(endpoint.joinRoom, {
+					method: "post",
+					body: { user, room }
+				}, (res) => {
+					this.room = room
+					this.choices = res.choices
+				})
+			}
+		}
+	})
 
+}
